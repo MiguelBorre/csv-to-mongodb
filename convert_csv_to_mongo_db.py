7df2d0bc52e46392053d0aed278451ba63010e0e
Miguel Borregón <miguel.borregon04@gmail.com>
2024-12-09 17:30:06 +0100

    fixed embebed documents

--- convert_csv_to_mongo_db.py
+++ convert_csv_to_mongo_db.py
@@ -1,45 +1,67 @@
-import pandas as pd
 from pathlib import Path
 import csv
 import json
 import sys
 import ast
 
-def read_csv_to_json (csv_file):
+def read_csv_to_json (csv_file: str, columns:dict):
+    """reads the csv file with the documents to be embbebed
+
+    Args:
+        csv_file (str): main csv file
+        columns (dict): dictionary with the name of the files of keys and (primary_key_field_name, foreing_key_field_name, json_name)
+
+    Returns:
+        dict: dict with all the data from the database
+    """
+    print("reading main csv")
+
+    out = list()
+
     with open(csv_file, mode='r', encoding='utf-8') as f:
-        columns = f.readline()
-        columns = columns.split(", ")
         reader = csv.DictReader(f)
         data = [row for row in reader]
 
-        # Evaluate the json columns, which are quoted, to convert them into a dictionary or list of dictionaries
-        # print(type(data[0]), data[0])
-        for row in data:
-            for col in columns:
-                try:
-                    # evaluate row[col] as dict or list of dicts
-                    row[col] = ast.literal_eval(row[col])
-                    # print(f"SETTING {col} TO {row[col]}")
-                except Exception as e:
-                    pass  # ignore - not a good practice, in general   
-        
-    return data
+        print("finished reading csv")
+
+        for fichero in columns.keys():
+            print(f"starting reading {fichero}")
+            with open(fichero, mode='r', encoding='utf-8') as f:
+                reader = csv.DictReader(f)
+                col_data = [row for row in reader]
+
+            index = {item[columns[fichero][1]]: item for item in col_data}
+
+            print(f"finished reading {fichero} \nImbibing the file to the field")
+
+            for row in data:
+                #we have to take the relation betwen the foreing key and the primary key to insert the document
+                primary_key = columns[fichero][0]
+                atribute_name = columns[fichero][2]
+                row[atribute_name] = index.get(row[primary_key])
+                del row[primary_key]
+                out.append(row)
+
+    return out
 
 def write_json(data, name):
+    print(f"writing file to {name}.json")
     json_file = Path(name).with_suffix('.json')
     with open(json_file, mode='w') as f:
         json.dump(data, f)
+    print("finished to write file")
 
 if __name__ == "__main__":
     csv_file = sys.argv[1]
     child_files = sys.argv[2:]
 
-    main_data = read_csv_to_json(csv_file)
+    child_dict = dict()
+    while len(child_files) >= 4:
+        head = child_files[:4]
+        child_dict[head[0]] = (head[1], head[2], head[3])
+        child_files = child_files[4:]
 
-    for fichero in child_files:
-        main_data[fichero] = read_csv_to_json(fichero)
+    main_data = read_csv_to_json(csv_file, child_dict)
 
     #simply writes the data filename with a json format and extension       
     write_json(main_data, csv_file)
-
-
